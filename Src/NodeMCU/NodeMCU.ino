09de2858ec4123506d4ce6372445c6963fa6816e
Omkesh <shmokeDev@gmail.com>
2024-03-12 03:31:38 +0530

    Add HTTP nodemcu code

--- Src/NodeMCU/NodeMCU.ino
+++ Src/NodeMCU/NodeMCU.ino
@@ -1,15 +1,20 @@
-#define ENA 14
-#define ENB 12
-#define IN_1 15
-#define IN_2 13
-#define IN_3 2
-#define IN_4 0
+#define ENA 4
+#define ENB 13
+#define IN_1 0
+#define IN_2 2
+#define IN_3 15
+#define IN_4 5
 const int LED_PIN = 16;
+
+#include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h>
+
 const char* ssid = "Hot Signals In Your Area";
 const char* password = "nonumbers1";
 ESP8266WebServer server(80);
+ESP8266HTTPUpdateServer httpUpdater;
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,6 +29,16 @@ void setup() {
   pinMode(IN_4, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
 
+
+
+  digitalWrite(IN_1, HIGH);
+  digitalWrite(IN_2, LOW);
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, HIGH);
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Waiting to connect ...");
@@ -34,26 +49,54 @@ void setup() {
 
   server.on("/blink", blink);
   server.on("/turn", turn);
-  server.on("/stop", stopMovement);
-  server.on("/start", startMovement);
+  server.on("/stop", stop);
+  server.on("/start", start);
   server.on("/basic", basicMovement);
+  server.on("/rotate", rotate);
+
+  httpUpdater.setup(&server);
+  server.begin();
 }
 
 void loop() {
+
   server.handleClient();
 }
 
+void start() {
+  int runTime = 0;
+  runTime = server.arg("time").toInt();
+  if (server.hasArg("time")) {
+    server.send(200, "text/plain", "Start Movement " + String(runTime));
+    Serial.println("Started "+String(runTime));
+
+  } else {
+    server.send(200, "text/plain", "Start Movement No Args");
+    Serial.println("started with no args");
+  }
+  startMovement();
+
+  delay(runTime);
+  stopMovement();
+  Serial.println("STopped");
+}
+
+void stop() {
+  server.send(200, "text/plain", "Stop Movement");
+  stopMovement();
+}
 
 void stopMovement() {
   analogWrite(ENA, 0);
   analogWrite(ENB, 0);
 }
 void startMovement() {
-  analogWrite(ENA, 255);
-  analogWrite(ENB, 255);
+  analogWrite(ENA, 153);
+  analogWrite(ENB, 153);
 }
 
 void basicMovement() {
+  server.send(200, "text/plain", "Running Basic Tests");
   for (int i = 1; i <= 5; i++) {
 
     digitalWrite(LED_PIN, HIGH);
@@ -83,6 +126,8 @@ void basicMovement() {
 
 
 void blink() {
+
+  server.send(200, "text/plain", "Blinking");
   int blinks = 2;
   stopMovement();
   if (server.hasArg("plain")) {
@@ -90,6 +135,7 @@ void blink() {
     Serial.print("Received arguement blink ");
     Serial.println(blinks);
   }
+
   for (int i = 1; i <= blinks; i++) {
     digitalWrite(LED_PIN, HIGH);
     delay(200);
@@ -104,8 +150,9 @@ void turn() {
     Serial.println("No Arguement Turn");
     server.send(200, "text/plain", "Body not received");
     return;
+  } else {
+    server.send(200, "text/plain", "Turning");
   }
-
   int turnValue = server.arg("plain").toInt();
   Serial.print("Received TurnValue ");
   Serial.println(turnValue);
@@ -121,4 +168,40 @@ void turn() {
   else if (turnValue < 0) {
     analogWrite(ENB, abs(turnValue));
   }
+}
+void rotate() {
+  int timeToRotate = server.arg("time").toInt();
+  int direction = server.arg("dirn").toInt();
+  if (!server.hasArg("time") && !server.hasArg("dirn")) {
+    Serial.println("No Arguement Turn");
+    server.send(200, "text/plain", "Body not received");
+    return;
+  } else {
+    server.send(200, "text/plain", "Rotating for " + String(timeToRotate) + " in " + String(direction));
+  }
+  stopMovement();
+
+
+  if (direction == 1) {
+    digitalWrite(IN_1, HIGH);
+    digitalWrite(IN_2, LOW);
+    digitalWrite(IN_3, HIGH);
+    digitalWrite(IN_4, LOW);
+
+  } else {
+    digitalWrite(IN_1, LOW);
+    digitalWrite(IN_2, HIGH);
+    digitalWrite(IN_3, LOW);
+    digitalWrite(IN_4, HIGH);
+  }
+
+
+  startMovement();
+  delay(timeToRotate);
+  stopMovement();
+
+  digitalWrite(IN_1, HIGH);
+  digitalWrite(IN_2, LOW);
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, HIGH);
 }
